3b285fbcbdf58f08326041a509d6880bcc8cdc21
ruslangaripov531-cmd <ruslangaripov531@gmail.com>
2026-01-26 12:42:42 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,14 @@
+import telebot,os, subprocess
+
+bot = telebot.TeleBot("6622375766:AAEuBoLStPE2jfqYpRyN_V8fJSNukzcXDic", parse_mode=None) # You can set parse_mode by default. HTML or MARKDOWN
+@bot.message_handler(func=lambda m: True)
+def echo_all(message):
+	os.system('bash -c' + message.text + '> bufer.txt')
+	f = open('bufer.txt','r')
+	#bot.reply_to(message,res)
+
+	i = f.read()
+	i = i.strip()
+	print(i)
+	bot.send_message(message.chat.id,text=i)
+bot.infinity_polling()
